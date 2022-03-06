6e09b8980be79cf2d899ae77c537690232b536c5
MinaAhmadi77 <mina412@aut.ac.ir>
2022-03-06 15:38:33 +0330

    finiiiiiished

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
 const formidable = require('formidable');
 const formidableMiddleware = require('express-formidable');
 
-// app.get('/', (req, res) => {
-//     res.send(`
-//       <h2>With <code>"express"</code> npm package</h2>
-//       <form action="/api/upload" enctype="multipart/form-data" method="post">
-//         <div>Text field title: <input type="text" name="title" /></div>
-//         <div>File: <input type="file" name="someExpressFiles" multiple="multiple" /></div>
-//         <input type="submit" value="Upload" />
-//       </form>
-//     `);
-// });
+
 
 // app.post('/api/upload', (req, res, next) => {
 //     const form = formidable({ multiples: true, uploadDir: __dirname + '/commentfile' });
